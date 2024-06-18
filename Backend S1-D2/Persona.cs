86f9a49db57c9d2927ccabf02e4f4a23bb78df97
Frattins <davide.frattini1@gmail.com>
2024-06-18 14:18:34 +0200

    Aggiungere i file di progetto.

---
+++ Backend S1-D2/Persona.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend_S1_D2
+{
+    internal class Persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public int Eta { get; set; }
+
+        public Persona (string nome, string cognome, int eta)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Eta = eta;
+        }
+
+        public string GetNome()
+        { 
+            return Nome; 
+        }
+
+        public string GetCognome()
+        {  return Cognome; }
+
+        public int GetEta()
+        { return Eta; }
+
+
+        public string GetDettagli()
+        {
+            return $"Nome: {Nome}, Cognome: {Cognome}, Età: {Eta}";
+        }
+    } 
+            
+    }
+
---
+++ Backend S1-D2/Program.cs
@@ -0,0 +1,16 @@
+﻿using Backend_S1_D2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Creazione dell'oggetto Persona
+        Persona persona = new Persona("Mario", "Rossi", 30);
+        Persona persona2 = new Persona("Davide", "Frattini", 25);
+
+        // Utilizzo dei metodi per stampare i dati
+
+        Console.WriteLine(persona.GetDettagli());
+        Console.WriteLine(persona2.GetDettagli());
+    }
+}
